4aaf5eaece6335ed51091bbdb9d36c7f180b94ee
iori <iori.sa1013@icloud.com>
2023-01-09 21:25:36 +0900

    commit lesson1~3.php

---
+++ 2_phpBasic/4_value_type_variable/lesson1.php
@@ -0,0 +1,5 @@
+<?php
+//$helloWorldという変数にHello Worldという文字列を格納し出力してください。
+$helloWorld = 'Hellow World'.'aaa';
+echo $helloWorld;
+?>
---
+++ 2_phpBasic/4_value_type_variable/lesson3.php
@@ -0,0 +1,18 @@
+﻿<?php
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+$personInfo = [
+  'name' => '山田',
+  'age' => '20',
+  'gender' => '女性'
+];
+
+// 山田
+// 20歳・女性
+echo $personInfo['name'];
+echo '<br/>';
+echo $personInfo['age'].'歳・'.$personInfo['gender'];
+
+// という形で出力してください。
