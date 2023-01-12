b75f95efed2d259e64d559d15acc039ff74ee679
iori <iori.sa1013@icloud.com>
2023-01-12 21:18:42 +0900

    commit 7_arithmetic-operators_answerfile

---
+++ 2_phpBasic/7_arithmetic_operators/lesson3.php
@@ -0,0 +1,13 @@
+﻿<?php
+// 100円のチョコレートを1個
+// 50円のアメを3個買いました。
+
+// 各値段の計算を変数に代入し、
+// その合計を$sumと言う変数に代入して
+// 「○○○円です。」という形で出力してください。
+$totalAmountChocolate = 100; 
+$totalAmountCandy = 50 * 3;
+
+$sum = $totalAmountChocolate + $totalAmountCandy;
+echo $sum.'円です。';
+?>
